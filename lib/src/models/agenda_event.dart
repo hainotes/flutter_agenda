817e90638749100c1d107baa9a5f481d7c6c260b
Hung Vu <hung.q.vu@thermofisher.com>
2022-07-20 23:30:14 -0700

    Converted to default text and subtitle style

--- lib/src/models/agenda_event.dart
+++ lib/src/models/agenda_event.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_agenda/src/models/event_time.dart';
 
 class AgendaEvent {
+  static const defaultTextStyle = const TextStyle(
+      color: Color(0xFF535353), fontSize: 11, fontWeight: FontWeight.w400);
+
+  static const defaultSubtitleStyle =
+      const TextStyle(fontWeight: FontWeight.w800, color: Color(0xFF363636));
+
   final String title;
 
   final String subtitle;
@@ -38,10 +44,8 @@ class AgendaEvent {
     this.onTap,
     this.decoration,
     this.backgroundColor: const Color(0xFF323D6C),
-    this.textStyle: const TextStyle(
-        color: Color(0xFF535353), fontSize: 11, fontWeight: FontWeight.w400),
-    this.subtitleStyle:
-        const TextStyle(fontWeight: FontWeight.w800, color: Color(0xFF363636)),
+    this.textStyle: defaultTextStyle,
+    this.subtitleStyle: defaultSubtitleStyle,
     this.builder,
   }) : assert(end.isAfter(start));
 }
